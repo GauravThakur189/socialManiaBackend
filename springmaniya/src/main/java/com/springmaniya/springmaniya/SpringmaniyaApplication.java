85681a0815ee93c5ad1a1df56ef4bcc18689cda0
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-09-07 18:38:06 +0530

    project setup

---
+++ springmaniya/src/main/java/com/springmaniya/springmaniya/SpringmaniyaApplication.java
@@ -0,0 +1,13 @@
+package com.springmaniya.springmaniya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmaniyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmaniyaApplication.class, args);
+	}
+
+}
---
+++ springmaniya/src/test/java/com/springmaniya/springmaniya/SpringmaniyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmaniya.springmaniya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmaniyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
