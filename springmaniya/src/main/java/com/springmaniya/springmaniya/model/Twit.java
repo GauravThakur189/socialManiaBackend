3b65824a4dd36f767c86a6f167ef355c72c63713
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-09-09 12:25:35 +0530

    Implemented model repository for twit and comment

--- springmaniya/src/main/java/com/springmaniya/springmaniya/model/Twit.java
+++ springmaniya/src/main/java/com/springmaniya/springmaniya/model/Twit.java
@@ -3,6 +3,7 @@ package com.springmaniya.springmaniya.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,4 +39,6 @@ public class Twit {
 
     private boolean isReply;
     private  boolean isTwit;
+
+    private LocalDateTime CreatedAt;
 }
---
+++ springmaniya/src/main/java/com/springmaniya/springmaniya/repository/TwitRepository.java
@@ -0,0 +1,20 @@
+package com.springmaniya.springmaniya.repository;
+
+import com.springmaniya.springmaniya.model.Twit;
+import com.springmaniya.springmaniya.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TwitRepository extends JpaRepository<Twit,Long> {
+
+    List<Twit> findAllByIsTwitTrueOrderByCreatedAtDesc();
+
+    List<Twit> findByRetwitUserContainsOrUser_IdAndIsTwitTrueOrderByCreatedAtDesc(User user,Long userId);
+
+    List<Twit> findByLikesContaingOrderByCreatedAtDesc(User user);
+
+    @Query("Select t From Twit t JOIN t.likes l where l.user.id=:userId")
+    List<Twit> findByLikesUser_id(Long userId);
+}
